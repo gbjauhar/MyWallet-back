5590f76abf5b58b696cb6f80f763bc34cff2f104
Gabriela Jauhar <gbjauhar@gmail.com>
2022-11-19 21:19:07 -0300

    feat: entry and exit posts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import express from "express"
-import cors from "cors"
-import joi from "joi"
-import dotenv from "dotenv"
-import bcrypt from "bcrypt"
-import { v4 as uuid } from 'uuid';
-import pkg from "mongodb"
-const {MongoClient} = pkg
-
-
+import express from "express";
+import cors from "cors";
+import joi from "joi";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import pkg from "mongodb";
+const { MongoClient } = pkg;
 
 const app = express();
 
@@ -22,57 +20,147 @@ mongoClient.connect().then(() => console.log("conectado"));
 mongoClient.connect().catch((err) => console.log(err));
 
 //database and collections
-let db = mongoClient.db("MyWallet")
-let users = db.collection("users")
-let entries = db.collection("entries")
-let exits = db.collection("exits")
+let db = mongoClient.db("MyWallet");
+let users = db.collection("users");
+let sessions = db.collection("sessions");
+let send = db.collection("send")
 
 //schemas
 
-const signUpSchema = joi.object({ 
+const signUpSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().required(),
-  password: joi.string().required()
- })
+  password: joi.string().required(),
+});
 
- const token = uuid();
- app.post("/signup", async (req, res) => {
+const sendSchema = joi.object({
+  cost: joi.string().required(),
+  description: joi.string().required(),
+});
 
+app.post("/signup", async (req, res) => {
   const user = req.body;
 
-  const {error} = signUpSchema.validate(user, {abortEarly: false})
-  if(error){
-    const errors = error.details.map((detail) => detail.message)
-    res.send(errors)
-    return
+  const { error } = signUpSchema.validate(user, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    res.send(errors);
+    return;
   }
-  
-  const passwordHash = bcrypt.hashSync(user.password, 10);
-  await users.insertOne({ ...user, password: passwordHash }) 
 
+  const passwordHash = bcrypt.hashSync(user.password, 10);
+  await users.insertOne({ ...user, password: passwordHash });
 
   res.sendStatus(201);
 });
 
 app.get("/signup", async (req, res) => {
-try{
-  const findUsers = await users.find().toArray()
-  res.send(findUsers)
-}catch(err){
-  res.sendStatus(500)
-}
-})
+  try {
+    const findUsers = await users.find().toArray();
+    res.send(findUsers);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
 
 app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  
+
   const user = await users.findOne({ email });
-  if(user && bcrypt.compareSync(password, user.password)) {
-    res.send("sucesso")
-    } else {
-      res.send("erro")
-        }
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = uuid();
+
+    await sessions.insertOne({
+      userId: user._id,
+      token,
+    });
+
+    res.send(token);
+  } else {
+    res.send("erro");
+  }
 });
+
+app.post("/entry", async (req, res) => {
+  const { cost, description } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const session = await sessions.findOne({ token });
+
+  if (!session) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const user = await users.findOne({
+    _id: session.userId,
+  });
+
+  const { error } = sendSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    res.send(errors);
+    return;
+  }
+  if (user) {
+    send.insertOne({
+      cost,
+      description,
+      type: "entry",
+    });
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.post("/exit", async (req, res) => {
+  const { cost, description } = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const session = await sessions.findOne({ token });
+
+  if (!session) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const user = await users.findOne({
+    _id: session.userId,
+  });
+
+  const { error } = sendSchema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    res.send(errors);
+    return;
+  }
+  if (user) {
+    send.insertOne({
+      cost,
+      description,
+      type: "exit",
+    });
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running in port: ${process.env.PORT}`)
